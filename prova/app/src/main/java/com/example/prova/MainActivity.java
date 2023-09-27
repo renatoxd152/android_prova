2f3ad17a7f128df28059a7ef8a2cb9756ef3ebfa
renato morillo <r.morillo@animalltag.com.br>
2023-09-27 11:19:04 -0300

    Sistema com banco

--- prova/app/src/main/java/com/example/prova/MainActivity.java
+++ prova/app/src/main/java/com/example/prova/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.prova;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -17,15 +19,23 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Texto> lista = new ArrayList<>();
     private AdapterTexto adapterTexto;
 
-
-
+    SQLiteDatabase bd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bd = openOrCreateDatabase("banco_prova",MODE_PRIVATE,null);
+
+        String cmd = "CREATE TABLE IF NOT EXISTS textos (id INTEGER PRIMARY KEY AUTOINCREMENT, NOME VARCHAR(100) NOT NULL, COR INT NOT NULL)";
+
+        bd.execSQL(cmd);
+
         listaTextos = findViewById(R.id.listaTextos);
         adapterTexto = new AdapterTexto(this,lista);
         listaTextos.setAdapter(adapterTexto);
+
+        carregarDadosDoBanco();
 
         EscutadorLista el = new EscutadorLista();
         listaTextos.setOnItemClickListener( el );
@@ -47,13 +57,41 @@ public class MainActivity extends AppCompatActivity {
             String texto1 = data.getStringExtra("texto");
             int corValor = data.getIntExtra("cor", 0);
             Texto texto = new Texto(texto1,corValor);
-
+            inserirTextoNoBanco(texto);
             lista.add(texto);
             adapterTexto.notifyDataSetChanged();
+
+
         }
 
     }
 
+
+    private void carregarDadosDoBanco() {
+        String query = "SELECT * FROM textos";
+        Cursor cursor = bd.rawQuery(query, null);
+
+        int idColumnnIndex = cursor.getColumnIndex("id");
+        int nomeColumnIndex = cursor.getColumnIndex("NOME");
+        int corColumnIndex = cursor.getColumnIndex("COR");
+
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(idColumnnIndex);
+                String nome = cursor.getString(nomeColumnIndex);
+                int cor = cursor.getInt(corColumnIndex);
+
+                Texto texto = new Texto(id,nome,cor);
+
+
+                lista.add(texto);
+            } while (cursor.moveToNext());
+
+            cursor.close();
+
+            adapterTexto.notifyDataSetChanged();
+        }
+    }
 
     private class EscutadorLista implements AdapterView.OnItemClickListener,
             AdapterView.OnItemLongClickListener {
@@ -80,13 +118,44 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+            int id = lista.get(i).getId();
+
+            removerTextodoBanco(id);
+
             lista.remove(i);
 
             adapterTexto.notifyDataSetChanged();
 
-            Toast.makeText(MainActivity.this, "Item apagado!", Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this, "Item Apagado!", Toast.LENGTH_LONG).show();
             return true;
         }
+
     }
+
+
+    public void inserirTextoNoBanco(Texto texto) {
+
+        bd = openOrCreateDatabase("banco_prova", MODE_PRIVATE, null);
+
+        String cmd = "INSERT INTO textos (NOME, COR) VALUES (?, ?)";
+        bd.execSQL(cmd, new Object[]{texto.getTexto(), texto.getCor()});
+
+        bd.close();
+    }
+
+
+    public void removerTextodoBanco(int id)
+    {
+        bd = openOrCreateDatabase("banco_prova", MODE_PRIVATE, null);
+
+        String cmd = "DELETE FROM textos WHERE id = ?";
+        bd.execSQL(cmd, new Object[]{id});
+
+        bd.close();
+    }
+
+
+
+
 
 }
--- prova/app/src/main/java/com/example/prova/Texto.java
+++ prova/app/src/main/java/com/example/prova/Texto.java
@@ -1,8 +1,20 @@
 package com.example.prova;
 
 public class Texto {
+
+    private int id;
     private String texto;
     private int cor;
+
+    public Texto(int id, String texto, int cor) {
+        this.id = id;
+        this.texto = texto;
+        this.cor = cor;
+    }
+
+    public int getId() {
+        return id;
+    }
 
     public Texto(String texto, int cor) {
         this.texto = texto;
